dd8bca5eab7fa7bdf3e11e129917ae9e2cc7a0de
GiovanniAmadei <giovannisebastianoamadei@gmail.com>
2024-07-24 22:40:13 +0200

    forse fix ma probabilmente no

---
+++ Source/SobekProject/SobekProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SobekProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SobekProject, "SobekProject" );
